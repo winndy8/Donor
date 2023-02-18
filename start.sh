06fa040f8db14342530ba145352acc64d0190bab
winndy8 <125177960+winndy8@users.noreply.github.com>
2023-02-18 12:05:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,2 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/winndy8/Donor.git /DQ-The-File-Donor
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
-fi
-cd /DQ-The-File-Donor
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
